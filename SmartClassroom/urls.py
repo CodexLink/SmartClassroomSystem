8d85897368e0fd6211ab6c12d61be29ddeae0e35
Janrey Licas <janjan.licas@gmail.com>
2020-01-09 22:36:06 +0800

    Django URLs | 10% Configuration

--- SmartClassroom/urls.py
+++ SmartClassroom/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', ITSOControl.site.urls),
+    path('staff/', StaffControl.site.urls),
 ]
