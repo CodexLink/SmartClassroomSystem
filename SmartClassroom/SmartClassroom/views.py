4ebeb1ba3dedb26b1fe33a5d67a0c13ad2457b66
Janrey Licas <janjan.licas@gmail.com>
2020-01-23 21:22:47 +0800

    Design } Prep To Design Cards

--- SmartClassroom/SmartClassroom/views.py
+++ SmartClassroom/SmartClassroom/views.py
@@ -1,12 +1,22 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
 import requests
+from requests.exceptions import ConnectionError
 
 # ! A Class That Just Loads the Default "home.html"
+
+
 class HomeFrameView(TemplateView):
 
     title_page = None
-    response = requests.get("http://192.168.100.41/RequestSens")
 
     def get(self, request):
-        return render(request, 'home.html', {"page_title": self.response.text, "page_type": "Admin"})
+        return render(request, 'home.html', {"page_title": self.sendMCUData(), "page_type": "Teacher"})
+
+    def sendMCUData(self):
+        try:
+            return requests.get("http://192.168.100.41/RequestSens", timeout=3)
+        except ConnectionError as Err:
+            return None
+            #self.response.text = None
+            pass
