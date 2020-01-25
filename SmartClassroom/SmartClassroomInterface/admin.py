07bd5bd67c0fb0986b3963d2ab1db6739b5783c9
Janrey Licas <janjan.licas@gmail.com>
2020-01-25 22:00:16 +0800

    Checkpoint | Remove Fields @ DJango-Admin
    Also, Fixed Relationship Compatibility on Some Fields
    - Fix Name with Automatic S appended

--- SmartClassroom/SmartClassroomInterface/admin.py
+++ SmartClassroom/SmartClassroomInterface/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class SmartClassroomAdmin(admin.ModelAdmin):
+    # ! We use this to hide our data from django-admin. No one will edit this one but can view it.
+    exclude = ('roomClassReferrable', 'secSubReferrable')
+
+admin.site.register((Classroom, CourseSet, ProgramBranch, SectionGroup, CourseSchedule, ClassroomLogger), SmartClassroomAdmin)
--- SmartClassroom/SmartClassroomInterface/models.py
+++ SmartClassroom/SmartClassroomInterface/models.py
@@ -7,13 +7,13 @@ class Classroom(models.Model):
     roomClassBuilding = models.PositiveIntegerField(null=False, blank=False, choices=BuildingClassification, validators=[MinValueValidator(1), MaxValueValidator(11)])
     roomClassFloor = models.PositiveIntegerField(null=False, blank=False, default=BuildingFloors[0], choices=BuildingFloors, validators=[MinValueValidator(0), MaxValueValidator(5)])
     roomClassNumber = models.PositiveIntegerField(null=False, blank=False, default=1, validators=[MinValueValidator(0), MaxValueValidator(29)])
-    roomClassType = models.CharField(max_length=15, null=False, blank=False, default=CourseSessionTypes[0], choices=CourseSessionTypes)
+    roomClassType = models.CharField(max_length=15, null=False, blank=False, unique=True, default=CourseSessionTypes[0], choices=CourseSessionTypes)
     roomClassReferrable = models.CharField(max_length=6, null=False, blank=False, unique=True, validators=[MinLengthValidator(6), MaxLengthValidator(6)])
 
     def __str__(self):
         return 'Room Q-{0}{1}{2}{3} | {4} Type | {5}'.format(self.roomClassBuilding, self.roomClassFloor, self.roomClassNumber, self.roomClassType, self.roomClassName)
 
-class CourseSets(models.Model):
+class CourseSet(models.Model):
     courseSts_Name = models.CharField(max_length=100, null=False, blank=False, validators=[MinLengthValidator(5), MaxLengthValidator(100)])
     courseSts_Code = models.CharField(max_length=10, null=False, blank=False, unique=True, primary_key=True, validators=[MinLengthValidator(4), MaxLengthValidator(10)])
     courseSts_Units = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(6)])
@@ -26,15 +26,15 @@ class CourseSets(models.Model):
     class Meta:
         unique_together = ('courseSts_Code', 'courseSts_Type')
 
-class ProgramClasses(models.Model):
+class ProgramBranch(models.Model):
     prog_name = models.CharField(max_length=50, null=False, blank=False, validators=[MinLengthValidator(5), MaxLengthValidator(50)])
     prog_description = models.TextField(max_length=255, null=True, blank=True, validators=[MinLengthValidator(10), MaxLengthValidator(255)])
     prog_code = models.CharField(primary_key=True, max_length=10, choices=CEAProgamCCode, unique=True, validators=[MinLengthValidator(4), MaxLengthValidator(10)])
 
-class SectionGroups(models.Model):
+class SectionGroup(models.Model):
     secBaseName = models.CharField(max_length=50, null=False, blank=False, validators=[MinLengthValidator(5), MaxLengthValidator(50)])
     secBaseDescrip = models.TextField(max_length=255, null=False, blank=True, validators=[MinLengthValidator(10), MaxLengthValidator(255)])
-    secBaseProg = models.OneToOneField(ProgramClasses, primary_key=True, to_field="prog_code" ,null=False, blank=False, unique=True, on_delete=models.CASCADE)
+    secBaseProg = models.OneToOneField(ProgramBranch, primary_key=True, to_field="prog_code" ,null=False, blank=False, unique=True, on_delete=models.CASCADE)
     secYear = models.PositiveIntegerField(choices=YearBatchClasses, null=False, blank=False, unique=True)
     secSem = models.CharField(max_length=15, null=False, blank=False, choices=SemClassification, unique=True)
     secSubUniqueSec = models.CharField(max_length=3, null=False, blank=False, choices=SubSectionUniqueKeys, unique=True)
@@ -46,8 +46,8 @@ class SectionGroups(models.Model):
     class Meta:
         unique_together = ('secBaseProg', 'secYear', 'secSem', 'secSubUniqueSec')
 
-class CourseSchedules(models.Model):
-    courseSch_CourseCode = models.OneToOneField(CourseSets, primary_key=True, to_field="courseSts_Code", null=False, blank=False, unique=True, on_delete=models.CASCADE)
+class CourseSchedule(models.Model):
+    courseSch_CourseCode = models.OneToOneField(CourseSet, primary_key=True, to_field="courseSts_Code", null=False, blank=False, unique=True, on_delete=models.CASCADE)
     courseSch_Handler = models.CharField(max_length=50, null=False, blank=False, unique=True, validators=[MinLengthValidator(4), MaxLengthValidator(50)])
     courseSch_DayLect = models.CharField(max_length=10, null=False, blank=False, default=SessionDaysClassification[0], choices=SessionDaysClassification, unique=True)
     courseSch_sched_startTime = models.TimeField(null=False, blank=False, unique=True)
@@ -63,9 +63,10 @@ class CourseSchedules(models.Model):
 # ! Required Logging for Admin | Teachers Actions on that Particular Classroom
 class ClassroomLogger(models.Model):
     ActionTaken = models.CharField(max_length=255, null=False, blank=False, choices=ClassroomActionTypes)
-    ClassroomRefer = models.ForeignKey(Classroom, null=False, blank=False, to_field="roomClassReferrable", on_delete=models.CASCADE)
-    CourseRefer = models.OneToOneField(CourseSchedules, null=False, blank=False, to_field="courseSch_CourseCode", related_name="CourseAssociated", on_delete=models.CASCADE)
-    SectionRefer = models.ForeignKey(SectionGroups, null=False, blank=False, to_field="secSubReferrable", related_name="StudentGroupAssociate", on_delete=models.CASCADE)
-    HandlerRefer = models.ForeignKey(CourseSchedules, null=False, blank=False, to_field="courseSch_Handler", related_name="HandlerAssociate", on_delete=models.CASCADE)
-    TimeStartRefer = models.ForeignKey(CourseSchedules, null=False, blank=False, to_field="courseSch_sched_startTime", related_name="TimeStartEvent", on_delete=models.CASCADE)
-    TimeEndRefer = models.ForeignKey(CourseSchedules, null=False, blank=False, to_field="courseSch_sched_startEnd", related_name="TimeEndEvent", on_delete=models.CASCADE)
+    ClassRoomRefer = models.ForeignKey(Classroom, null=False, blank=False, to_field="roomClassReferrable", related_name="RoomClassReference", on_delete=models.CASCADE)
+    ClassTypeRefer = models.OneToOneField(Classroom, null=False, blank=False, to_field="roomClassType", related_name="TypeClassReference", on_delete=models.CASCADE)
+    CourseRefer = models.OneToOneField(CourseSchedule, null=False, blank=False, to_field="courseSch_CourseCode", related_name="CourseAssociated", on_delete=models.CASCADE)
+    SectionRefer = models.ForeignKey(SectionGroup, null=False, blank=False, to_field="secSubReferrable", related_name="StudentGroupAssociate", on_delete=models.CASCADE)
+    HandlerRefer = models.ForeignKey(CourseSchedule, null=False, blank=False, to_field="courseSch_Handler", related_name="HandlerAssociate", on_delete=models.CASCADE)
+    TimeStartRefer = models.ForeignKey(CourseSchedule, null=False, blank=False, to_field="courseSch_sched_startTime", related_name="TimeStartEvent", on_delete=models.CASCADE)
+    TimeEndRefer = models.ForeignKey(CourseSchedule, null=False, blank=False, to_field="courseSch_sched_startEnd", related_name="TimeEndEvent", on_delete=models.CASCADE)
