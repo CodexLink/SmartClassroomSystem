f7db5248feea68228c22b6dc9d11541484a087da
Janrey Licas <janjan.licas@gmail.com>
2020-01-27 21:03:42 +0800

    URL | Added Names On 3rd Argument

--- SmartClassroom/SCControlSystem/urls.py
+++ SmartClassroom/SCControlSystem/urls.py
@@ -18,12 +18,12 @@ from django.urls import include, path
 from .views import *
 
 urlpatterns = [
-    path('', HomeView.as_view()),
-    path('login/', AuthUserView.as_view()),
-    path('logout/', DeauthUserView.as_view()),
+    path('', HomeView.as_view(), name='front_project_view'),
+    path('login/', AuthUserView.as_view(), name='auth_user_view'),
+    path('logout/', DeauthUserView.as_view(), name='deauth_user_view'),
     path('dashboard/', DashboardView.as_view(), name='dashboard_user_view'),
     path('classroom/', ClassroomView.as_view(), name='classroom_user_view'),
-    # ! Check for possible options here. n URL.
+    # ! Check for possible options here, URL Parameters.
     path('classroom/logs/', SelectableClassroomView.as_view(), name='classroom_log_extensible_view'), # ! Extensible means usable as whole page or modular component.
     # ! Not confirmed yet.
     path('classroom/<str:clasroom_number>/logs/', SelectableClassroomView.as_view(path_action='check_log', as_modular_view=False), name='classroom_log_extensible_view'), # ! Extensible means usable as whole page or modular component.
@@ -32,5 +32,5 @@ urlpatterns = [
     path('classroom/<str:clasroom_number>/actions/', SelectableClassroomView.as_view(path_action='get_actions'), name='classroom_action_view'),
     path('schedule/', ScheduleListView.as_view(), name='schedule_exclusive_view'), # ! For teachers only.
     path('override/', OverrideControlView.as_view(), name='override_exclusive_view'),
-    path('system/', SystemSettingsView.as_view(), name='override_exclusive_view')
+    path('system/', SystemSettingsView.as_view(), name='syssettings_exclusive_view')
 ]
