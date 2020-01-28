d42bb54d864573fa07e30f56889ea2b028539eda
Janrey Licas <janjan.licas@gmail.com>
2020-01-28 21:05:10 +0800

    Static | Updated Collected Static Files

---
+++ SmartClassroom/app_static_src/js/sc_require.js
@@ -0,0 +1,25 @@
+/*
+    ! sc_require.js
+    * Created by Janrey Licas
+    * Created on 01/24/2019
+    * This was added to make front-end customizations and other such stuff such as AJAX.
+*/
+
+// ! Mansory Dashboard View
+$('.dashboard-grid').isotope({
+    itemSelector: '.dashboard-item',
+    layoutMode: 'packery',
+    packery: {
+        columnWidth: '.dashboard-item',
+        horizontal: false
+    },
+    percentPosition: true,
+});
+
+// ! Update View ACcording to Layout Changes Since Card is at Absolute Position
+$('.quick-actions-handler').click(function (event) {
+    setTimeout(function ()
+    {
+        $('.dashboard-grid').isotope("arrange");
+    }, 200);
+});
