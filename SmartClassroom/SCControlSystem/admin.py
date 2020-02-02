d3b4eec159bc0d879317cfd4459b818d60e2f3d2
Janrey Licas <janjan.licas@gmail.com>
2020-02-02 21:50:40 +0800

    Admin | Normalize Fields with Helper Text

--- SmartClassroom/SCControlSystem/admin.py
+++ SmartClassroom/SCControlSystem/admin.py
@@ -9,17 +9,32 @@ class ModelAttributes(admin.ModelAdmin):
     # ! We use this to hide our data from django-admin. No one will edit this one but can view it.
     model = UserDataCredentials
     exclude = ('Classroom_CompleteString', 'Section_CompleteStringGroup')
-    #list_display = ()  # Contain only fields in your `custom-user-model`
-    #list_filter = ()  # Contain only fields in your `custom-user-model` intended for filtering. Do not include `groups`since you do not have it
-    #search_fields = ()  # Contain only fields in your `custom-user-model` intended for searching
-    #ordering = ()  # Contain only fields in your `custom-user-model` intended to ordering
-    #filter_horizontal = () # Leave it empty. You have neither `groups` or `user_permissions`
-    #fieldsets = UserAdmin.fieldsets + (
+    # list_display = ()  # Contain only fields in your `custom-user-model`
+    # list_filter = ()  # Contain only fields in your `custom-user-model` intended for filtering. Do not include `groups`since you do not have it
+    # search_fields = ()  # Contain only fields in your `custom-user-model` intended for searching
+    # ordering = ()  # Contain only fields in your `custom-user-model` intended to ordering
+    # filter_horizontal = () # Leave it empty. You have neither `groups` or `user_permissions`
+    # fieldsets = UserAdmin.fieldsets + (
     #        (None, {'fields': ('mobile',)}),
-    #)
+    # )
+
 
 class UserClassAttributes(UserAdmin):
-    pass
+    model = UserDataCredentials
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Security Unique Assets', {
+            'fields': ('Common_ID', 'Unique_ID'),
+            }
+        ),
+        ('Additional Personal info', {
+            'fields': ('Middle_Name', 'User_Role'),
+            }
+        ),
+    )
+    readonly_fields = ('Common_ID', 'Unique_ID',)
+
+
 
 admin.site.register((Classroom,
                      Course,
