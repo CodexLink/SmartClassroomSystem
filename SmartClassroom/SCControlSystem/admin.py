abc1b42f15b138db86a7475e33761a37fecddb3d
Janrey Licas <janjan.licas@gmail.com>
2020-02-07 01:47:18 +0800

    >Admin | Better Displays
    Make Customized User Model literally respectable.

--- SmartClassroom/SCControlSystem/admin.py
+++ SmartClassroom/SCControlSystem/admin.py
@@ -9,7 +9,6 @@ class ModelAttributes(admin.ModelAdmin):
     # ! We use this to hide our data from django-admin. No one will edit this one but can view it.
     model = UserDataCredentials
     exclude = ('Classroom_CompleteString', 'Section_CompleteStringGroup')
-    # list_display = ()  # Contain only fields in your `custom-user-model`
     # list_filter = ()  # Contain only fields in your `custom-user-model` intended for filtering. Do not include `groups`since you do not have it
     # search_fields = ()  # Contain only fields in your `custom-user-model` intended for searching
     # ordering = ()  # Contain only fields in your `custom-user-model` intended to ordering
@@ -20,7 +19,21 @@ class ModelAttributes(admin.ModelAdmin):
 
 
 class UserClassAttributes(UserAdmin):
-    model = UserDataCredentials
+    model = UserDataCredentials # ! A Model or DB to reference at.
+
+    """
+        Types of Features in DJango-Admin
+        List_Display makes you able to display their content by referencing a particular field.
+        List_Filter is a feature exclusive based on referenced field. In short, you can filter on **that** field.
+
+    """
+    list_display = ('first_name', 'Middle_Name', 'last_name', 'User_Role', 'is_active' ,'is_staff', 'is_superuser', 'date_joined',)  # Contain only fields in your `custom-user-model`
+    list_filter = ('first_name', 'Middle_Name', 'last_name', 'User_Role', 'is_active' ,'is_staff', 'is_superuser', 'date_joined',)  # Contain only fields in your `custom-user-model`
+    search_fields = ('first_name', 'Middle_Name', 'last_name', 'User_Role', 'is_active' ,'is_staff', 'is_superuser', 'date_joined',)  # Contain only fields in your `custom-user-model`
+
+    # ! Fieldsets is container that contains fields that can be read / modified.
+    # * The reason why we did UserAdmin.fieldsets + because we just want to have additional items.
+    # ! The default ones will be retained.
 
     fieldsets = UserAdmin.fieldsets + (
         ('Security Unique Assets', {
