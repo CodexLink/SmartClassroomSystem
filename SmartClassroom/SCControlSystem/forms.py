de3a04f26c4ddbdc1f6df988aadd3d27157e1393
Janrey Licas <janjan.licas@gmail.com>
2020-02-07 23:54:42 +0800

    Forms | Change from TextInput to PasswordInput

--- SmartClassroom/SCControlSystem/forms.py
+++ SmartClassroom/SCControlSystem/forms.py
@@ -4,13 +4,13 @@ from .externs.subject_types import RoleDeclaredTypes
 from .models import UserDataCredentials
 from django.contrib.auth.forms import AuthenticationForm
 
-class UserAuthForm(AuthenticationForm):
+class UserAuthForm(AuthenticationForm, forms.ModelForm):
     class Meta:
         model = UserDataCredentials
         fields = ['username', 'password']
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control', 'id': 'validationUsername', 'placeholder': 'Required'}),
-            'password': forms.TextInput(attrs={'type': 'password', 'class': 'form-control', 'id': 'validationPassword', 'placeholder': 'Required'}),
+            'password': forms.PasswordInput(attrs={'type': 'password', 'class': 'form-control', 'id': 'validationPassword', 'placeholder': 'Required'}),
         }
     username = forms.CharField(min_length=2, max_length=150, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'validationUsername', 'placeholder': 'Required'}))
-    password = forms.CharField(min_length=2, max_length=128, required=True, widget=forms.TextInput(attrs={'type': 'password', 'class': 'form-control', 'id': 'validationPassword', 'placeholder': 'Required'}))
+    password = forms.CharField(min_length=2, max_length=128, required=True, widget=forms.PasswordInput(attrs={'type': 'password', 'class': 'form-control', 'id': 'validationPassword', 'placeholder': 'Required'}))
