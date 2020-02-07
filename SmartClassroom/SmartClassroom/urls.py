87c03396b217219c2c2aaa9320bd03b987a53e7d
Janrey Licas <janjan.licas@gmail.com>
2020-02-08 00:00:19 +0800

    Avatar Support | Finalized Except Placeholder Image

--- SmartClassroom/SmartClassroom/urls.py
+++ SmartClassroom/SmartClassroom/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
 
     # path('admin/<str:AdminKey>/', include('AdminRoomControl.urls')), ! To Be Used for Certain Logins and other such stuff.
     path('', include('SCControlSystem.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
