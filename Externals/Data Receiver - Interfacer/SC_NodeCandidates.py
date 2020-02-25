0bb349a08539993fa10b8b081ac0678a080cec7b
Janrey Licas <janjan.licas@gmail.com>
2020-02-25 16:51:40 +0800

    Node Candidates  | Removed '|' for User Login

--- Externals/Data Receiver - Interfacer/SC_NodeCandidates.py
+++ Externals/Data Receiver - Interfacer/SC_NodeCandidates.py
@@ -4,6 +4,6 @@
     Here's the structure: {'Device_Name': 'IP_ADDRESS (IPV4 ONLY)'}
 '''
 NodeDevCandidates = {
-    'NodeMCU | Q-9309': '192.168.100.41',
-    'NodeMCU | Q-XXX1': '1',
+    'NodeMCU Q-5424': '192.168.100.41',
+    'NodeMCU Q-XXX1': '1',
 }
