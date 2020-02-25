c04c6167ff650f5572f1b5957fc3a847b9e92334
Janrey Licas <janjan.licas@gmail.com>
2020-02-25 16:57:03 +0800

    Sketch Library Update | Checkpoint
    I cannot add anymore description because it wastes my time more than I should at coding.

--- Externals/Sketch/AVR_RoomIoT/SmartClassroom.cpp
+++ Externals/Sketch/AVR_RoomIoT/SmartClassroom.cpp
@@ -1,22 +1,44 @@
 #include "SmartClassroom.h"
 
+SoftwareSerial FP_WIRE(SC_MCU_DRVR::SENS_DAT_PINS_PUBLIC::FNGR_RX_PIN, SC_MCU_DRVR::SENS_DAT_PINS_PUBLIC::FNGR_TX_PIN);
+GT5X FPController(&FP_WIRE);
+GT5X_DeviceInfo FP_DEV_INFO;
+
 SC_MCU_DRVR::SC_MCU_DRVR(uint16_t BAUD_RATE, const char *SSID, const char *PW)
 {
     __BAUD_RATE = BAUD_RATE;
-    strcpy(WIFI_SSID, SSID);
-    strcpy(WIFI_PW, PW);
+    strcpy(WIFI_INST_STRUCT.WIFI_SSID, SSID);
+    strcpy(WIFI_INST_STRUCT.WIFI_PW, PW);
 }
 
 void SC_MCU_DRVR::begin()
 {
     Serial.begin(__BAUD_RATE);
     LCD_DRVR.begin();
-    WiFi.begin(WIFI_SSID, WIFI_PW); //WiFi connection
-    TempSens.begin();
+    WiFi.begin(WIFI_INST_STRUCT.WIFI_SSID, WIFI_INST_STRUCT.WIFI_PW); //WiFi connection
+    FP_WIRE.begin(__BAUD_RATE);
+    FPController.begin(&FP_DEV_INFO);
+    FPController.set_led(false);
+
+    pinMode(RESTATED_DEV_PINS::ESP_LED, OUTPUT);
+    pinMode(RESTATED_DEV_PINS::MCU_LED, OUTPUT);
+
+    pinMode(SENS_DAT_PINS::PIR_DAT_PIN, OUTPUT);
+    TempSens.setup(SENS_DAT_PINS::TEMP_HUD_DAT_PIN, DHTesp::DHT11);
+
+    pinMode(SENS_DAT_PINS_PUBLIC::RELAY_FRST_PIN, OUTPUT);
+    digitalWrite(SENS_DAT_PINS_PUBLIC::RELAY_FRST_PIN, HIGH);
+
+    pinMode(SENS_DAT_PINS_PUBLIC::RELAY_SCND_PIN, OUTPUT);
+    digitalWrite(SENS_DAT_PINS_PUBLIC::RELAY_SCND_PIN, HIGH);
+
+    pinMode(SENS_DAT_PINS_PUBLIC::RELAY_THRD_PIN, OUTPUT);
+    digitalWrite(SENS_DAT_PINS_PUBLIC::RELAY_THRD_PIN, HIGH);
     LCD_DRVR.noBacklight();
     LCD_DRVR.clear();
     delay(500);
     LCD_DRVR.backlight();
+    FPController.set_led(true);
     return;
 }
 
@@ -25,17 +47,24 @@ bool SC_MCU_DRVR::checkWiFiConnection()
     displayLCDScreen(DataDisplayTypes::CLEAR);
     displayLCDScreen(DataDisplayTypes::WAITPOINT);
     Serial.print("Connection to WiFi is not established. Waiting.");
+    digitalWrite(RESTATED_DEV_PINS::MCU_LED, HIGH);
     while (WiFi.status() != WL_CONNECTED)
     {
+        digitalWrite(RESTATED_DEV_PINS::ESP_LED, LOW);
         Serial.print(".");
-        delay(1000);
+        delay(400);
+        digitalWrite(RESTATED_DEV_PINS::ESP_LED, HIGH);
+        delay(400);
     }
+    digitalWrite(RESTATED_DEV_PINS::ESP_LED, HIGH);
     Serial.println();
     Serial.print("Connected to ");
-    Serial.print(WIFI_SSID);
-    Serial.print("!!! | Reference |> ");
+    Serial.print(WIFI_INST_STRUCT.WIFI_SSID);
+    Serial.print("!!! | IP Address |> ");
     Serial.println(WiFi.localIP());
-    displayLCDScreen(DataDisplayTypes::CLEAR);
+    displayLCDScreen(DataDisplayTypes::WAIT_CLEAR);
+    delay(1000);
+    displayLCDScreen(DataDisplayTypes::CLEAR_OFF_DISP);
     return true;
 }
 
@@ -47,7 +76,7 @@ bool SC_MCU_DRVR::mntndWiFiConnection()
     }
     else
     {
-        Serial.println("Connection to WiFi is lost. Awaiting Connection...");
+        Serial.println("Connection to WiFi was lost. Attempting ReConnection...");
         return (checkWiFiConnection() ? true : false);
     }
 }
@@ -57,46 +86,65 @@ void SC_MCU_DRVR::displayLCDScreen(DataDisplayTypes Screens)
     switch (Screens)
     {
     case DataDisplayTypes::WAITPOINT:
-
         LCD_DRVR.setCursor(0, 0);
-        LCD_DRVR.print("Smart Classroom");
+        LCD_DRVR.print("Smart Classroom Sys.");
         LCD_DRVR.setCursor(0, 1);
-        LCD_DRVR.print("Connecting...");
+        LCD_DRVR.print(" Ver. 02222020-2327");
+        LCD_DRVR.setCursor(0, 2);
+        LCD_DRVR.print(" Interactless Mgmt.");
+        LCD_DRVR.setCursor(0, 3);
+        LCD_DRVR.print(" Connecting to SSID");
         break;
+
+    case DataDisplayTypes::WAIT_CLEAR:
+        LCD_DRVR.setCursor(0, 3);
+        LCD_DRVR.print("     Connected!     ");
+        break;
+
     case DataDisplayTypes::CLEAR:
         LCD_DRVR.clear();
         break;
+
     case DataDisplayTypes::CLEAR_OFF_DISP:
         LCD_DRVR.noBacklight();
         LCD_DRVR.clear();
         delay(500);
         LCD_DRVR.backlight();
         break;
-    case DataDisplayTypes::DISP_DATA:
-        DHT_Temp = TempSens.readTemperature();
-        DHT_Humid = TempSens.readHumidity();
-        DHT_HTIndx = TempSens.computeHeatIndex(DHT_Temp, DHT_Humid, false);
+
+    case DataDisplayTypes::DISP_CR_INFO:
+        digitalWrite(RESTATED_DEV_PINS::MCU_LED, LOW);
+        ENV_INST_CONT.DHT11_TEMP = TempSens.getTemperature();
+        ENV_INST_CONT.DHT11_HUMID = TempSens.getHumidity();
+        ENV_INST_CONT.DHT11_HT_INDX = TempSens.computeHeatIndex(ENV_INST_CONT.DHT11_TEMP, ENV_INST_CONT.DHT11_HUMID, false);
         LCD_DRVR.setCursor(0, 0);
-        LCD_DRVR.print("TEMP: ");
-        LCD_DRVR.print(DHT_Temp);
-        LCD_DRVR.print("C");
+        LCD_DRVR.print(DEV_INST_CREDENTIALS.DEV_CR_ASSIGNED);
+        LCD_DRVR.print(F(" | "));
+        LCD_DRVR.print(DEV_INST_CREDENTIALS.DEV_CR_ROOM);
         LCD_DRVR.setCursor(0, 1);
-        LCD_DRVR.print("HT IDX: ");
-        LCD_DRVR.print(DHT_HTIndx);
-        LCD_DRVR.print("C");
+        LCD_DRVR.print(F("S:"));
+        LCD_DRVR.print((AUTH_INST_CONT.AUTH_CR_DOOR) ? "Lockd" : "UnLkd");
+        LCD_DRVR.print(F(" | T:"));
+        LCD_DRVR.print(ENV_INST_CONT.DHT11_TEMP, 1);
+        LCD_DRVR.print(F("C"));
         LCD_DRVR.setCursor(0, 2);
-        LCD_DRVR.print("HUMID: ");
-        LCD_DRVR.print(DHT_Humid);
-        LCD_DRVR.print("%");
-        Serial.print("REPORT > TEMP: ");
-        Serial.print(DHT_Temp);
-        Serial.print("C | HT INDEX: ");
-        Serial.print(DHT_HTIndx);
-        Serial.print("C | HUMID: ");
-        Serial.print(DHT_Humid);
+        LCD_DRVR.print(F("H:"));
+        LCD_DRVR.print(ENV_INST_CONT.DHT11_HUMID, 1);
+        LCD_DRVR.print(F("% | HTI:"));
+        LCD_DRVR.print(ENV_INST_CONT.DHT11_HT_INDX, 1);
+        LCD_DRVR.print(F("C"));
+
+        Serial.print("DEV REPORT | T, P: ");
+        Serial.print(ENV_INST_CONT.DHT11_TEMP);
+        Serial.print("C | HTI: ");
+        Serial.print(ENV_INST_CONT.DHT11_HT_INDX);
+        Serial.print("C | HUD: ");
+        Serial.print(ENV_INST_CONT.DHT11_HUMID);
         Serial.println("%");
+        digitalWrite(RESTATED_DEV_PINS::MCU_LED, HIGH);
         break;
-    case DataDisplayTypes::DISP_NEXT_SCHED:
+
+    case DataDisplayTypes::DEBUG_FNGRPRNT_ENROLL:
         LCD_DRVR.setCursor(0, 0);
         //LCD_DRVR.print();
         //LCD_DRVR.print();
@@ -112,8 +160,131 @@ void SC_MCU_DRVR::displayLCDScreen(DataDisplayTypes Screens)
         //LCD_DRVR.print();
         //LCD_DRVR.print();
         break;
+
+    case DataDisplayTypes::DEBUG_FNGRPRNT_VERIFY:
+        LCD_DRVR.setCursor(0, 0);
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        LCD_DRVR.setCursor(0, 1);
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        LCD_DRVR.setCursor(0, 2);
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        break;
+
+    case DataDisplayTypes::DEBUG_MCU_GENERALIZED:
+        LCD_DRVR.setCursor(0, 0);
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        LCD_DRVR.setCursor(0, 1);
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        LCD_DRVR.setCursor(0, 2);
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        //LCD_DRVR.print();
+        break;
+
     default:
         break;
     }
+    return;
+}
+
+void SC_MCU_DRVR::authCheck_Fngrprnt()
+{
+    if (FPController.is_pressed() && AUTH_INST_CONT.AUTH_CR_ACCESS)
+    {
+        uint16_t rc = FPController.capture_finger();
+        if (rc != GT5X_OK)
+        {
+            return;
+        }
+
+        rc = FPController.verify_finger_with_template(AUTH_INST_CONT.AUTH_USER_ID_FNGRPRNT);
+
+        if (rc != GT5X_OK)
+        {
+            digitalWrite(SENS_DAT_PINS_PUBLIC::RELAY_FRST_PIN, HIGH);
+            digitalWrite(SENS_DAT_PINS_PUBLIC::RELAY_SCND_PIN, HIGH);
+            digitalWrite(SENS_DAT_PINS_PUBLIC::RELAY_THRD_PIN, HIGH);
+            LCD_DRVR.setCursor(0, 3);
+            AUTH_INST_CONT.NON_AUTH_ELECTRIC_STATE = false;
+            AUTH_INST_CONT.AUTH_CR_DOOR = true;
+            LCD_DRVR.print(F("> Invalid UID!"));
+            return;
+        }
+        else
+        {
+            digitalWrite(SENS_DAT_PINS_PUBLIC::RELAY_FRST_PIN, LOW);
+            digitalWrite(SENS_DAT_PINS_PUBLIC::RELAY_SCND_PIN, LOW);
+            digitalWrite(SENS_DAT_PINS_PUBLIC::RELAY_THRD_PIN, LOW);
+            AUTH_INST_CONT.AUTH_CR_DOOR = false;
+            AUTH_INST_CONT.NON_AUTH_ELECTRIC_STATE = true;
+            LCD_DRVR.setCursor(0, 3);
+            LCD_DRVR.print(F("> Verified UID: "));
+            LCD_DRVR.print(AUTH_INST_CONT.AUTH_USER_ID_FNGRPRNT);
+            return;
+            // Add Relay ON State.
+        }
+    }
+    else
+    {
+        LCD_DRVR.setCursor(0, 3);
+        if (!AUTH_INST_CONT.AUTH_CR_ACCESS)
+        {
+            LCD_DRVR.print(F("> Access Disabled!  "));
+        }
+        else
+        {
+            LCD_DRVR.print(F("> Ready.            "));
+        }
+        return;
+    }
+}
+
+inline bool SC_MCU_DRVR::FNGRPRNT_isLEDState(bool whatState, bool changeOnFalse)
+{
+    bool retState = (whatState == AUTH_INST_CONT.AUTH_FP_LED_STATE) ? true : false;
+    Serial.print(F("FNGRPRNT_isLEDState | "));
+    Serial.print(F("Given State |> "));
+    Serial.print(whatState);
+    Serial.print(F(" | changedOnFalse |> "));
+    Serial.print(changeOnFalse);
+    Serial.print(F(" | funcReturn |> "));
+    Serial.println(retState);
+
+    // If we dont need to change anything and we just want the state then just return bool.
+    if (!changeOnFalse)
+    {
+        return retState;
+    }
+    else // We return the same thing from above except we do some extra steps.
+    {
+        if (!retState)
+            FNGRPRNT_InverseLEDState();
+    }
+}
+
+inline void SC_MCU_DRVR::FNGRPRNT_InverseLEDState()
+{
+    Serial.print(F("FNGRPRNT_InverseLEDState | "));
+    Serial.print(F("Before |> "));
+    Serial.print(AUTH_INST_CONT.AUTH_FP_LED_STATE);
+    Serial.print(F(" | After |> "));
+    Serial.println(!AUTH_INST_CONT.AUTH_FP_LED_STATE);
+    // Inverse the output whenever this function was executed.
+    AUTH_INST_CONT.AUTH_FP_LED_STATE = !AUTH_INST_CONT.AUTH_FP_LED_STATE;
+    FPController.set_led(AUTH_INST_CONT.AUTH_FP_LED_STATE);
     return;
 }
--- Externals/Sketch/AVR_RoomIoT/SmartClassroom.h
+++ Externals/Sketch/AVR_RoomIoT/SmartClassroom.h
@@ -5,24 +5,31 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-//#include "ArduinoJSON.h"
-#include "DHT.h"
+#include <ArduinoOTA.h>
+#include "DHTesp.h"
+#include <SoftwareSerial.h>
 #include "LiquidCrystal_I2C.h"
+#include "GT5X.h"
 
+#define TX_OVERRIDE 3
+#define SSD3 10
 class SC_MCU_DRVR
 {
     // ! Container that can be to reference all constant values.
-    enum ConstantsVal : uint8_t
+    enum CONST_VAL
     {
         NULL_CONTENT = 0,
         MAX_REL_CHANNEL = 5,
         MAX_IP_ADDR_CHAR = 15,
         MAX_WIFI_SSD_CHAR = 32,
         MAX_WIFI_PW_CHAR = 63,
-        ESP_DEFAULT_PORT = 80
+        ESP_DEFAULT_PORT = 80,
+        MAX_STR_CR = 20,
+        MAX_STR_UUID = 40,
+        MAX_FNGRPRNT_STORABLE = 3000,
     };
 
-    enum ConstantsDev : uint8_t
+    enum CONST_DEV_PARAM
     {
         LCD_ADDR = 0x27,
         LCD_W = 20,
@@ -30,69 +37,114 @@ class SC_MCU_DRVR
     };
 
     // * This Container utilizes PINs to be used to scan the whole room.
-    enum SensDataPins : uint8_t
+    enum SENS_DAT_PINS
     {
-        PIR_DAT_PIN = 12,
-        TEMP_HUD_DAT_PIN = 14,
-        FNGR_DAT_PIN = NULL_CONTENT
+        TEMP_HUD_DAT_PIN = D0,
+        PIR_DAT_PIN = D1,
+        DEMUX_LTCH_PIN = 0,
+        DEMUX_CLK_PIN = 0,
     };
 
+    struct WIFI_CRENDENTIALS
+    {
+        char WIFI_SSID[MAX_WIFI_SSD_CHAR];
+        char WIFI_PW[MAX_WIFI_PW_CHAR];
+    } WIFI_INST_STRUCT;
+
     // ! Variables
-    uint16_t __BAUD_RATE = NULL_CONTENT;
-    uint8_t REL_TRIGGER[ConstantsVal::MAX_REL_CHANNEL] = {NULL_CONTENT}; // ! We'll be using 4-channel relay module
-
-    char WIFI_SSID[ConstantsVal::MAX_WIFI_SSD_CHAR] = {NULL_CONTENT};
-    char WIFI_PW[ConstantsVal::MAX_WIFI_PW_CHAR] = {NULL_CONTENT};
-
-    // To be use when DJango sends GET request.
-
-    // ! Environmental Container Variables
-    float DHT_Temp = NULL_CONTENT;
-    float DHT_Humid = NULL_CONTENT;
-    float DHT_HTIndx = NULL_CONTENT;
+    uint16_t __BAUD_RATE = CONST_VAL::NULL_CONTENT;
+    uint8_t RELAY_TRIGGER[CONST_VAL::MAX_REL_CHANNEL] = {CONST_VAL::NULL_CONTENT}; // ! We'll be using 4-channel relay module
 
     // ! Object (Class) Instance
-    LiquidCrystal_I2C LCD_DRVR{ConstantsDev::LCD_ADDR, ConstantsDev::LCD_W, ConstantsDev::LCD_H};
-    DHT TempSens{SensDataPins::TEMP_HUD_DAT_PIN, DHT22};
-    ESP8266WebServer NodeServer{ConstantsVal::ESP_DEFAULT_PORT};
+    LiquidCrystal_I2C LCD_DRVR{CONST_DEV_PARAM::LCD_ADDR, CONST_DEV_PARAM::LCD_W, CONST_DEV_PARAM::LCD_H};
+    DHTesp TempSens;
+    ESP8266WebServer NodeServer{CONST_VAL::ESP_DEFAULT_PORT};
 
 public:
     // * Declares Data Interpretation To Be Used As A Parameter Later.
-    enum DataInterpretTypes
-    {
-        AUTH_TYPE,
-        SENS_TYPE,
-        REL_TYPE
-    };
-
     enum DataDisplayTypes
     {
         INITIALIZE,
         CLEAR,
         CLEAR_OFF_DISP,
         WAITPOINT,
-        DISP_DATA,
-        DISP_NEXT_SCHED,
-        DISP_TIMEOUT
+        WAIT_CLEAR,
+        DISP_CR_INFO,
+        DEBUG_FNGRPRNT_ENROLL,
+        DEBUG_FNGRPRNT_VERIFY,
+        DEBUG_MCU_GENERALIZED, // Generalized Test. Meaning MCU and with its components are being tested.
+
     };
 
-    String DictContainer = "";
+    enum RESTATED_DEV_PINS
+    {
+        ESP_LED = D4,
+        MCU_LED = D0
+
+    };
+
+    enum SENS_DAT_PINS_PUBLIC
+    {
+        FNGR_TX_PIN = D5,
+        FNGR_RX_PIN = D6,
+        RELAY_FRST_PIN = D7,
+        RELAY_SCND_PIN = TX_OVERRIDE,
+        RELAY_THRD_PIN = SSD3,
+        //RELAY_FRTH_PIN = D8,  Unused due to PIN Pulled LOW which would result to device not botting up.
+    };
+
+    // ! Environmental Module Outputs
+    struct ENV_MOD_OTPT
+    {
+        float DHT11_TEMP;
+        float DHT11_HUMID;
+        float DHT11_HT_INDX;
+        uint8_t PIR_OPTPT;
+        uint32_t PIR_MILLIS_TRIGGER;
+    } ENV_INST_CONT;
+
+    struct AUTH_STATE
+    {
+        uint8_t AUTH_FGPRT_STATE; // 1 Passed, 0 Not Passed
+
+        bool AUTH_CR_DOOR = 1;        // 1 Locked, 0 Unlocked
+        bool AUTH_CR_ACCESS = 1;      // 1 Enabled, 0 Disabled
+        bool NON_AUTH_ELECTRIC_STATE = 0;      // 1 Enabled, 0 Disabled
+        bool AUTH_FP_LED_STATE; // LED ON, LED OFF
+        uint16_t AUTH_USER_ID_FNGRPRNT = 1; // Must be set by user.
+        //int16_t AUTH_USER_ID_FNGRPRNTS[10] = -1;
+    } AUTH_INST_CONT;
+
+    const struct DEV_CREDENTIALS
+    {
+        char *DEV_CR_ASSIGNED;
+        char * DEV_CR_ROOM;
+        char *DEV_UID;
+        char *AUTH_DEV_USN;
+        char *AUTH_DEV_PWD;
+    } DEV_INST_CREDENTIALS
+    {
+
+        .DEV_CR_ASSIGNED = "Q-5424",
+        .DEV_CR_ROOM = "CompEng Lab", // Lckd, Used
+        .DEV_UID = "df826e0334b84f2689e64f2c6b24a6ab",
+        .AUTH_DEV_USN = "NodeMCU | Q-5424",
+        .AUTH_DEV_PWD = "df826e0334b84f2689e64f2c6b24a6ab",
+    };
 
     // General Function To Be Used.
     SC_MCU_DRVR(uint16_t BAUD_RATE, const char *SSID, const char *PW);
     void begin();
-    bool checkWiFiConnection();
     bool mntndWiFiConnection();
     void displayLCDScreen(DataDisplayTypes Screens);
-    // Fingerprint Function
-    void sendAuthenticate();
+    void authCheck_Fngrprnt();
+    //void
 
 private:
-    void scanSens();
-    void WaitForData();
-
-    void SendData(uint8_t TimeToSend);
-    void InterpretData(DataInterpretTypes DataType);
+    bool checkWiFiConnection();
+    inline bool FNGRPRNT_isLEDState(bool whatState, bool changeOnFalse);
+    inline void FNGRPRNT_InverseLEDState();
+    //void InterpretData(DataInterpretTypes DataType);
 };
 
 // END OF FILE SMARTROOM
