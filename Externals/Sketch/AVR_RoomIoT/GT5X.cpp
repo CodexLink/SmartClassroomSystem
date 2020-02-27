fd1853b59055e0d54679719335bf4f0973edee5f
Janrey Licas <janjan.licas@gmail.com>
2020-02-27 15:14:24 +0800

    GTX5 | Prettified

--- Externals/Sketch/AVR_RoomIoT/GT5X.cpp
+++ Externals/Sketch/AVR_RoomIoT/GT5X.cpp
@@ -1,6 +1,6 @@
 /* Written by Brian Ejike (2018) brianrho94@gmail.com
  * Distributed under the terms of the MIT license */
- 
+
 #include <Arduino.h>
 #include "GT5X.h"
 
@@ -12,11 +12,11 @@
     #define GT5X_DEBUG_DEC(x)            GT5X_DEFAULT_STREAM.print(x)
     #define GT5X_DEBUG_DECLN(x)          GT5X_DEFAULT_STREAM.println(x)
     #define GT5X_DEBUG_HEX(x)            GT5X_DEFAULT_STREAM.print(x, HEX)
-    #define GT5X_DEBUG_HEXLN(x)          GT5X_DEFAULT_STREAM.println(x, HEX) 
+    #define GT5X_DEBUG_HEXLN(x)          GT5X_DEFAULT_STREAM.println(x, HEX)
 #else
     #define GT5X_DEBUG_PRINT(x)
     #define GT5X_DEBUG_PRINTLN(x)
-    #define GT5X_DEBUG_DEC(x)          
+    #define GT5X_DEBUG_DEC(x)
     #define GT5X_DEBUG_DECLN(x)
     #define GT5X_DEBUG_HEX(x)
     #define GT5X_DEBUG_HEXLN(x)
@@ -31,22 +31,22 @@ typedef enum {
     GT5X_STATE_READ_CHECKSUM
 } GT5X_State;
 
-void GT5X::write_cmd_packet(uint16_t cmd, uint32_t params) {   
-    uint8_t preamble[] = {GT5X_CMD_START_CODE1, GT5X_CMD_START_CODE2, 
+void GT5X::write_cmd_packet(uint16_t cmd, uint32_t params) {
+    uint8_t preamble[] = {GT5X_CMD_START_CODE1, GT5X_CMD_START_CODE2,
                           (uint8_t)GT5X_DEVICEID, (uint8_t)(GT5X_DEVICEID >> 8)};
-    
+
     uint16_t chksum = 0;
     for (int i = 0; i < sizeof(preamble); i++) {
         chksum += preamble[i];
     }
-    
+
     memcpy(buffer, &params, sizeof(params));
     memcpy(buffer + sizeof(params), &cmd, sizeof(cmd));
-    
+
     for (int i = 0; i < GT5X_PARAM_CMD_LEN; i++) {
         chksum += buffer[i];
     }
-    
+
     port->write(preamble, sizeof(preamble));
     port->write(buffer, GT5X_PARAM_CMD_LEN);
     port->write((uint8_t *)&chksum, 2);
@@ -54,75 +54,75 @@ void GT5X::write_cmd_packet(uint16_t cmd, uint32_t params) {
 
 /* Any output parameter (or error code) is stored right back into params
    and the Response ACK/NACK is returned */
-   
+
 uint16_t GT5X::get_cmd_response(uint32_t * params) {
     GT5X_State state = GT5X_STATE_READ_HEADER;
     const uint16_t COMBINED_PACKET_HEADER = ((uint16_t)GT5X_CMD_START_CODE1 << 8) | GT5X_CMD_START_CODE2;
-    
+
     uint16_t header = 0;
     uint16_t rcode = 0;
     uint32_t last_read = millis();
-    
+
     while ((uint32_t)(millis() - last_read) < GT5X_DEFAULT_TIMEOUT) {
         yield();
-        
+
         switch (state) {
             case GT5X_STATE_READ_HEADER: {
                 if (port->available() == 0)
                     continue;
-                
+
                 last_read = millis();
                 uint8_t byte = port->read();
                 header <<= 8; header |= byte;
                 if (header != COMBINED_PACKET_HEADER)
                     break;
-                
+
                 state = GT5X_STATE_READ_DEVID;
                 header = 0;
-                
+
                 GT5X_DEBUG_PRINTLN("\r\n[+]Got header");
                 break;
             }
             case GT5X_STATE_READ_DEVID: {
                 if (port->available() < 2)
                     continue;
-                
+
                 last_read = millis();
                 uint16_t devid;
                 port->readBytes((uint8_t *)&devid, 2);
-                
+
                 /* check device id */
                 if (devid != GT5X_DEVICEID) {
                     state = GT5X_STATE_READ_HEADER;
                     GT5X_DEBUG_PRINTLN("[+]Wrong device ID");
                     break;
                 }
-                
+
                 state = GT5X_STATE_READ_PARAMS;
                 GT5X_DEBUG_PRINT("[+]ID: 0x"); GT5X_DEBUG_HEXLN(devid);
-                
+
                 break;
             }
             case GT5X_STATE_READ_PARAMS:
                 if (port->available() < 4)
                     continue;
-                
+
                 /* store output parameter or error code */
                 last_read = millis();
                 port->readBytes((uint8_t *)params, 4);
-                
+
                 state = GT5X_STATE_READ_RESPONSE;
                 GT5X_DEBUG_PRINT("[+]Params: 0x"); GT5X_DEBUG_HEXLN(*params);
-                
+
                 break;
             case GT5X_STATE_READ_RESPONSE: {
                 if (port->available() < 2)
                     continue;
-                
+
                 /* read ACK/NACK */
                 last_read = millis();
                 port->readBytes((uint8_t *)&rcode, 2);
-                
+
                 state = GT5X_STATE_READ_CHECKSUM;
                 GT5X_DEBUG_PRINT("[+]Response code: "); GT5X_DEBUG_DECLN(rcode);
                 break;
@@ -130,35 +130,35 @@ uint16_t GT5X::get_cmd_response(uint32_t * params) {
             case GT5X_STATE_READ_CHECKSUM: {
                 if (port->available() < 2)
                     continue;
-                
+
                 last_read = millis();
                 uint16_t temp;
                 port->readBytes((uint8_t *)&temp, 2);
-                
+
                 uint16_t chksum = GT5X_CMD_START_CODE1 + GT5X_CMD_START_CODE2
                                   + (uint8_t)GT5X_DEVICEID + (GT5X_DEVICEID >> 8);
-                
+
                 /* 4 from size of params */
                 for (int i = 0; i < 4; i++) {
                     chksum += ((uint8_t *)params)[i];
                 }
-                
+
                 chksum += rcode >> 8;
                 chksum += (uint8_t)rcode;
-                
+
                 /* compare chksum */
                 if (temp != chksum) {
                     state = GT5X_STATE_READ_HEADER;
                     GT5X_DEBUG_PRINTLN("\r\n[+]Wrong chksum");
                     continue;
                 }
-                
+
                 GT5X_DEBUG_PRINTLN("\r\n[+]Read complete");
                 return rcode;
             }
         }
     }
-    
+
     GT5X_DEBUG_PRINTLN("[+]Timeout.");
     return GT5X_TIMEOUT;
 }
@@ -166,60 +166,60 @@ uint16_t GT5X::get_cmd_response(uint32_t * params) {
 uint16_t GT5X::get_data_response(uint8_t * data, uint16_t len, Stream * outStream) {
     GT5X_State state = GT5X_STATE_READ_HEADER;
     const uint16_t COMBINED_PACKET_HEADER = ((uint16_t)GT5X_DATA_START_CODE1 << 8) | GT5X_DATA_START_CODE2;
-    
+
     uint16_t header = 0;
-    
+
     uint16_t remn = len;
     uint32_t last_read = millis();
-    
+
     while ((uint32_t)(millis() - last_read) < GT5X_DEFAULT_TIMEOUT) {
         yield();
-        
+
         switch (state) {
             case GT5X_STATE_READ_HEADER: {
                 if (port->available() == 0)
                     continue;
-                
+
                 last_read = millis();
                 uint8_t byte = port->read();
                 header <<= 8; header |= byte;
                 if (header != COMBINED_PACKET_HEADER)
                     break;
-                
+
                 state = GT5X_STATE_READ_DEVID;
                 header = 0;
-                
+
                 GT5X_DEBUG_PRINTLN("\r\n[+]Got header");
                 break;
             }
             case GT5X_STATE_READ_DEVID: {
                 if (port->available() < 2)
                     continue;
-                
+
                 last_read = millis();
                 uint16_t devid;
                 port->readBytes((uint8_t *)&devid, 2);
-                
+
                 if (devid != GT5X_DEVICEID) {
                     state = GT5X_STATE_READ_HEADER;
                     GT5X_DEBUG_PRINTLN("[+]Wrong device ID");
                     break;
                 }
-                
+
                 state = GT5X_STATE_READ_DATA;
                 GT5X_DEBUG_PRINT("[+]ID: 0x"); GT5X_DEBUG_HEXLN(devid);
-                
+
                 break;
             }
             case GT5X_STATE_READ_DATA: {
                 uint16_t avail = port->available();
                 uint16_t to_read;
-                
+
                 if (avail == 0)
                     continue;
-                
+
                 last_read = millis();
-                
+
                 if (outStream == NULL) {
                     to_read = (avail < remn) ? avail : remn;
                     port->readBytes(data, to_read);
@@ -231,82 +231,82 @@ uint16_t GT5X::get_data_response(uint8_t * data, uint16_t len, Stream * outStrea
                     port->readBytes(buffer, to_read);
                     outStream->write(buffer, to_read);
                 }
-                
+
                 remn -= to_read;
-                
+
                 if (remn == 0) {
                     state = GT5X_STATE_READ_CHECKSUM;
                     GT5X_DEBUG_PRINT("[+]Read len: "); GT5X_DEBUG_DECLN(len);
                 }
-                
+
                 break;
             }
             case GT5X_STATE_READ_CHECKSUM: {
                 if (port->available() < 2)
                     continue;
-                
+
                 last_read = millis();
                 uint16_t temp;
                 port->readBytes((uint8_t *)&temp, 2);
-                
+
                 if (outStream == NULL) {
                     uint16_t chksum = GT5X_DATA_START_CODE1 + GT5X_DATA_START_CODE2
                                       + (uint8_t)GT5X_DEVICEID + (GT5X_DEVICEID >> 8);
-                    
-                    /* walk backwards thru the data and add */                    
+
+                    /* walk backwards thru the data and add */
                     for (int i = 0; i < len; i++) {
                         chksum += *(--data);
                     }
-                    
+
                     if (temp != chksum) {
                         state = GT5X_STATE_READ_HEADER;
                         GT5X_DEBUG_PRINTLN("\r\n[+]Wrong chksum");
                         continue;
                     }
                 }
-                
+
                 GT5X_DEBUG_PRINTLN("\r\n[+]Read complete");
                 return len;
             }
         }
     }
-    
+
     GT5X_DEBUG_PRINTLN();
     return GT5X_TIMEOUT;
 }
 
-GT5X::GT5X(Stream * ss) : port(ss) 
+GT5X::GT5X(Stream * ss) : port(ss)
 {
-    
+
 }
 
 bool GT5X::begin(GT5X_DeviceInfo * info) {
     uint16_t cmd = GT5X_OPEN;
     uint32_t params = 1;
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
-    
+
     if (rc != GT5X_ACK)
         return false;
     else if (rc == GT5X_TIMEOUT)
         return rc;
-    
+
     rc = get_data_response((uint8_t *)&devinfo, sizeof(GT5X_DeviceInfo));
     if (rc != sizeof(GT5X_DeviceInfo))
         return false;
-    
+
     if (info != NULL) {
         memcpy(info, &devinfo, sizeof(GT5X_DeviceInfo));
     }
-    
+
     return true;
 }
 
 bool GT5X::end(void) {
     uint16_t cmd = GT5X_CLOSE;
     uint32_t params = 0;
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
     return (rc == GT5X_ACK);
@@ -315,30 +315,30 @@ bool GT5X::end(void) {
 uint16_t GT5X::set_led(bool state) {
     uint16_t cmd = GT5X_CMOSLED;
     uint32_t params = state ? 1 : 0;
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
     if (rc == GT5X_ACK)
         return GT5X_OK;
     else if (rc == GT5X_TIMEOUT)
         return rc;
-    
+
     return params;
 }
 
-/* trust the device to handle invalid rates, 
+/* trust the device to handle invalid rates,
  * will need to call end() and begin() after this */
 uint16_t GT5X::set_baud_rate(uint32_t baud) {
     uint16_t cmd = GT5X_CHANGEBAUDRATE;
     uint32_t params = baud;
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
     if (rc == GT5X_ACK)
         return GT5X_OK;
     else if (rc == GT5X_TIMEOUT)
         return rc;
-    
+
     /* returns the NACK error code, same purpose in other functions */
     return params;
 }
@@ -347,7 +347,7 @@ uint16_t GT5X::set_baud_rate(uint32_t baud) {
 uint16_t GT5X::get_enrolled_count(uint16_t * fcnt) {
     uint16_t cmd = GT5X_GETENROLLCNT;
     uint32_t params = 0;
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
     if (rc == GT5X_ACK) {
@@ -356,7 +356,7 @@ uint16_t GT5X::get_enrolled_count(uint16_t * fcnt) {
     }
     else if (rc == GT5X_TIMEOUT)
         return rc;
-    
+
     return params;
 }
 
@@ -365,14 +365,14 @@ uint16_t GT5X::get_enrolled_count(uint16_t * fcnt) {
 uint16_t GT5X::is_enrolled(uint16_t fid) {
     uint16_t cmd = GT5X_CHECKENROLLED;
     uint32_t params = fid;
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
     if (rc == GT5X_ACK)
         return GT5X_OK;
     else if (rc == GT5X_TIMEOUT)
         return rc;
-    
+
     return params;
 }
 
@@ -383,23 +383,23 @@ uint16_t GT5X::is_enrolled(uint16_t fid) {
 uint16_t GT5X::start_enroll(uint16_t fid) {
     uint16_t cmd = GT5X_STARTENROLL;
     uint32_t params = fid;
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
     if (rc == GT5X_ACK)
         return GT5X_OK;
     else if (rc == GT5X_TIMEOUT)
         return rc;
-    
+
     return params;
 }
 
 /** Scan finger for enrollment
- *  
+ *
  */
 uint16_t GT5X::enroll_scan(uint8_t pass) {
     uint16_t cmd;
-    
+
     switch (pass) {
         case 1:
             cmd = GT5X_ENROLL1;
@@ -411,7 +411,7 @@ uint16_t GT5X::enroll_scan(uint8_t pass) {
             cmd = GT5X_ENROLL3;
             break;
     }
-    
+
     uint32_t params = 0;
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
@@ -419,14 +419,14 @@ uint16_t GT5X::enroll_scan(uint8_t pass) {
         return GT5X_OK;
     else if (rc == GT5X_TIMEOUT)
         return rc;
-    
+
     return params;
 }
 
 bool GT5X::is_pressed(void) {
     uint16_t cmd = GT5X_ISPRESSFINGER;
     uint32_t params = 0;
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
     if (rc == GT5X_ACK) {
@@ -439,28 +439,28 @@ bool GT5X::is_pressed(void) {
 uint16_t GT5X::delete_id(uint16_t fid) {
     uint16_t cmd = GT5X_DELETEID;
     uint32_t params = fid;
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
     if (rc == GT5X_ACK)
         return GT5X_OK;
     else if (rc == GT5X_TIMEOUT)
         return rc;
-    
+
     return params;
 }
 
 uint16_t GT5X::empty_database(void) {
     uint16_t cmd = GT5X_DELETEALL;
     uint32_t params = 0;
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
     if (rc == GT5X_ACK)
         return GT5X_OK;
     else if (rc == GT5X_TIMEOUT)
         return rc;
-    
+
     return params;
 }
 
@@ -468,21 +468,21 @@ uint16_t GT5X::empty_database(void) {
 uint16_t GT5X::verify_finger_with_template(uint16_t fid) {
     uint16_t cmd = GT5X_VERIFY1_1;
     uint32_t params = fid;
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
     if (rc == GT5X_ACK)
         return GT5X_OK;
     else if (rc == GT5X_TIMEOUT)
         return rc;
-    
+
     return params;
 }
 
 uint16_t GT5X::search_database(uint16_t * fid) {
     uint16_t cmd = GT5X_IDENTIFY1_N;
     uint32_t params = 0;
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
     if (rc == GT5X_ACK) {
@@ -491,111 +491,111 @@ uint16_t GT5X::search_database(uint16_t * fid) {
     }
     else if (rc == GT5X_TIMEOUT)
         return rc;
-    
+
     return params;
 }
 
 uint16_t GT5X::capture_finger(bool highquality) {
     uint16_t cmd = GT5X_CAPTUREFINGER;
     uint32_t params = highquality ? 1 : 0;
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
     if (rc == GT5X_ACK)
         return GT5X_OK;
     else if (rc == GT5X_TIMEOUT)
         return rc;
-    
+
     return params;
 }
 
 uint16_t GT5X::get_template(uint16_t fid) {
     uint16_t cmd = GT5X_GETTEMPLATE;
     uint32_t params = fid;
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
     if (rc == GT5X_ACK)
         return GT5X_OK;
     else if (rc == GT5X_TIMEOUT)
         return rc;
-    
+
     return params;
 }
 
 uint16_t GT5X::get_image(void) {
     uint16_t cmd = GT5X_GETRAWIMAGE;
     uint32_t params = 0;
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
     if (rc == GT5X_ACK)
         return GT5X_OK;
     else if (rc == GT5X_TIMEOUT)
         return rc;
-    
+
     return params;
 }
 
 uint16_t GT5X::set_template(uint16_t fid, uint8_t check_duplicate) {
     uint16_t cmd = GT5X_SETTEMPLATE;
     uint32_t params = check_duplicate ? fid : (fid | 0xff000000);
-    
+
     write_cmd_packet(cmd, params);
     uint16_t rc = get_cmd_response(&params);
     if (rc == GT5X_ACK)
         return GT5X_OK;
     else if (rc == GT5X_TIMEOUT)
         return rc;
-    
+
     return params;
 }
 
 bool GT5X::read_raw(uint8_t outType, void * out, uint16_t to_read) {
     Stream * outStream;
     uint8_t * outBuf;
-    
+
     if (outType == GT5X_OUTPUT_TO_BUFFER)
         outBuf = (uint8_t *)out;
     else if (outType == GT5X_OUTPUT_TO_STREAM)
         outStream = (Stream *)out;
     else
         return false;
-    
+
     uint16_t rc;
-    
+
     if (outType == GT5X_OUTPUT_TO_BUFFER)
         rc = get_data_response(outBuf, to_read);
     else if (outType == GT5X_OUTPUT_TO_STREAM)
         rc = get_data_response(NULL, to_read, outStream);
-    
+
     /* check the length */
     if (rc != to_read) {
         GT5X_DEBUG_PRINT("Read data failed: ");
         GT5X_DEBUG_PRINTLN(rc);
         return false;
     }
-    
+
     return true;
 }
 
 uint16_t GT5X::write_raw(uint8_t * data, uint16_t len, bool expect_response) {
-    uint8_t preamble[] = {GT5X_DATA_START_CODE1, GT5X_DATA_START_CODE2, 
+    uint8_t preamble[] = {GT5X_DATA_START_CODE1, GT5X_DATA_START_CODE2,
                           (uint8_t)GT5X_DEVICEID, (uint8_t)(GT5X_DEVICEID >> 8)};
-    
+
     uint16_t chksum = 0;
     for (int i = 0; i < sizeof(preamble); i++) {
         chksum += preamble[i];
     }
-    
+
     for (int i = 0; i < len; i++) {
         chksum += data[i];
     }
-    
+
     port->write(preamble, sizeof(preamble));
     port->write(data, len);
     port->write((uint8_t *)&chksum, 2);
-    
+
     if (expect_response) {
         uint32_t params = 0;
         uint16_t rc = get_cmd_response(&params);
@@ -603,9 +603,9 @@ uint16_t GT5X::write_raw(uint8_t * data, uint16_t len, bool expect_response) {
             return GT5X_OK;
         else if (rc == GT5X_TIMEOUT)
             return rc;
-        
+
         return params;
     }
-    
+
     return GT5X_OK;
 }
