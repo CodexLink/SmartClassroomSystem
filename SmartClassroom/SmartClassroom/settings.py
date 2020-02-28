92d48f78de60b96dc71cec08336dc539a007dfbe
Janrey Licas <janjan.licas@gmail.com>
2020-02-28 19:51:45 +0800

    Settings | Added 'NodeHandler.apps.NodehandlerConfig'

--- SmartClassroom/SmartClassroom/settings.py
+++ SmartClassroom/SmartClassroom/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'SCControlSystem.apps.SmartClassroomConfig',
+    'NodeHandler.apps.NodehandlerConfig',
     'django_extensions',
 ]
 
