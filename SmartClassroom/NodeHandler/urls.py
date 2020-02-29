860aec0b4ce2c99e9b1ef63641abc13496fec626
Janrey Licas <janjan.licas@gmail.com>
2020-02-29 22:10:01 +0800

    URLs and Tests | Remove Tests and Let One Official Declare for URLs

--- SmartClassroom/NodeHandler/urls.py
+++ SmartClassroom/NodeHandler/urls.py
@@ -3,10 +3,9 @@ from django.urls import include, path
 from .views import *
 
 urlpatterns = [
-    #path('authenticate_node/<>/<>', AuthenticateNode.as_view(), name='auth_node_class'),
-
-    # This view is correlated at changing EEPRO of a targeted node.
-    #path('migrate_node/<int:>/<str:>/<int:>/<str:>', MigrationScheduler.as_view(), name='auth_user_view'),
-    #path(' /', .as_view(), name='deauth_user_view'),
-
+    #path('migrate_node/<int:>/<str:>/<int:>/<str:>', MigrationScheduler.as_view(), name='node_migration_processor'),
+    #path('/lockRemoteCall/', NodeMultiAuthenticator.as_view(), name='node_locker_processor'),
+    #path('/nodeMigrationer/', MigrationScheduler.as_view(), name='node_migration_processor'),
+    # ! This path has been called by lock. When nodemcu lock was on, then electricity and lock are both triggered.
+    path('lockRemoteCall/<str:NodeDevUUID>/<str:ClassroomUUID>/<int:user_fngrprnt_id>/LockState=<int:GivenContextLockState>', NodeMultiAuthenticator.as_view(), name='node_locker_processor'),
 ]
