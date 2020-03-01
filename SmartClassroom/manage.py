24c62df878cc287932b63254c7034b344d729dc9
Janrey Licas <janjan.licas@gmail.com>
2020-03-01 14:37:32 +0800

    SCControlSystem Manage | Added OS.SYSTEM Title

--- SmartClassroom/manage.py
+++ SmartClassroom/manage.py
@@ -8,6 +8,7 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SmartClassroom.settings')
     try:
         from django.core.management import execute_from_command_line
+        os.system("title SmartClassroom Django Server Handler")
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
