20cdfdd1f3587a9bdeda5dd11ae691064c113c28
Janrey Licas <janjan.licas@gmail.com>
2020-05-11 12:50:16 +0800

    Settings.PY | Added Placeholder

--- SmartClassroom/SmartClassroom/settings.py
+++ SmartClassroom/SmartClassroom/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ## See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 ## SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = None # SECRET_PRODUCTION_KEY
+SECRET_KEY = "qwertyuiopasdfghjkl;zxcvbnm,./qwertyuiop[" # SECRET_PRODUCTION_KEY
 
 ## SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
